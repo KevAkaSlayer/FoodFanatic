d791478ffb0acdf2de173c204b825911172b4566
Aminul Islam <142468113+KevAkaSlayer@users.noreply.github.com>
2024-02-28 16:08:53 +0600

    Delete FoodFanatic-main directory

--- FoodFanatic-main/menu/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class ReviewForm(forms.Form):
-    review = forms.CharField(widget=forms.Textarea)
--- FoodFanatic-main/order/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-admin.site.register(models.Order)
-admin.site.register(models.OrderItem)
-admin.site.register(models.SpecialOffer)
