e2943791a2f20992b16078c92033c9862f7383f6
KevAkaSlayer <pukman41@gmail.com>
2024-02-28 16:13:27 +0600

    slightly changed

--- FoodFanatic-main/user/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class AccountModel(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='account')
-    is_activated = models.BooleanField(default=False,null=True , blank = True)
-    email_token= models.CharField(max_length=200,null=True , blank = True)
