426895f7ecea9381d38a85525700c6d7f0316fa1
KevAkaSlayer <pukman41@gmail.com>
2024-03-04 00:52:32 +0600

    Changes
    Designed login, sign up and banner section, cart section of food fanatic

--- order/urls.py
+++ order/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('placeorder/', views.place_order,name = 'placeorder'),
-    path('order_details/<int:id>', views.order_details,name = 'order_details'),
+    path('order_details/<int:order_id>', views.order_details,name = 'order_details'),
     path('product_list', views.product_list,name = 'product_list'),
 ]
